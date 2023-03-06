3d53a4fed8b1515887484371bf011b610e76ecaf
OlenkaPasko <lena_nechipurenko@ukr.net>
2023-03-06 14:12:50 +0200

    open modal

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,79 +1,55 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
-
+import { galleryItems } from "./gallery-items.js";
 console.log(galleryItems);
-/*Створення і рендер розмітки на підставі масиву даних galleryItems 
-і наданого шаблону елемента галереї.
-Реалізація делегування на div.gallery і отримання url великого зображення.
-Підключення скрипту і стилів бібліотеки модального вікна basicLightbox. 
-Використовуй CDN сервіс jsdelivr і 
-додай у проект посилання на мініфіковані(.min) файли бібліотеки.
-Відкриття модального вікна по кліку на елементі галереї. 
-Для цього ознайомся з документацією і прикладами.
-Заміна значення атрибута src елемента < img >
-в модальному вікні перед відкриттям.
-Використовуй готову розмітку модального вікна із 
-зображенням з прикладів бібліотеки basicLightbox.*/
 
-const instance = basicLightbox.create(
-  `<img width="1280" height="auto" src="">`
-);
 const gallery = document.querySelector(".gallery");
-const createGallery = createGalleryItems(galleryItems);
-gallery.insertAdjacentHTML("beforeend", createGallery);
 
-function createGalleryItems(items) {
-  return galleryItems
-    .map(({ preview, original, description }) => {
-      return `<div class="gallery__item">
-  <a class="gallery__link" href="${original}">
+const createGallery = (items) => {
+  return items
+    .map(
+      (item) =>
+        `<div class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
     <img
       class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
     />
   </a>
-</div>`;
-    })
+</div>`
+    )
     .join("");
-}
-
-
-//const markup = newTechnologies
-  //.map((technology) => `<li class="list-item new">${technology}</li>`)
-  //.join("");
-
-
-/*<div class="gallery__item">
-  <a class="gallery__link" href="large-image.jpg">
-    <img
-      class="gallery__image"
-      src="small-image.jpg"
-      data-source="large-image.jpg"
-      alt="Image description"
-    />
-  </a>
-</div>;
-const createImage = (item, parent) => {
-  const { preview, original, description } = item;
-  const img = document.createElement('img');
-  
-  img.classList.add('gallery__image');
-  img.dataset.source = original;
-  img.src = preview;
-  img.alt = description;
-  
-  parent.appendChild(img);
 };
 
-const list = document.querySelector(".list");
+//create modal
+const addGallery = createGallery(galleryItems);
+gallery.innerHTML = addGallery;
 
-const newTechnologies = ["React", "TypeScript", "Node.js"];
-const markup = newTechnologies
-  .map((technology) => `<li class="list-item new">${technology}</li>`)
-  .join("");
+gallery.addEventListener("click", openModal);
 
-list.insertAdjacentHTML("beforeend", markup);
-list.insertAdjacentHTML("beforebegin", "<h2>Popular technologies</h2>");
-console.log(_)*/
+function openModal(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const instance = basicLightbox.create(
+    `<img src="${event.target.dataset.source}">`,
+    {
+      onShow: (instance) => {
+        window.addEventListener("keydown", onLinkClick);
+      },
+      onClose: (instance) => {
+        window.removeEventListener("keydown", onLinkClick);
+      },
+    }
+  );
+
+  instance.show();
+}
+function onLinkClick(event) {
+  if (event.code === "Escape") return; 
+    instance.close();
+  }
+
