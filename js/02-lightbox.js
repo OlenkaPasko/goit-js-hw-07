9f6f14f46d803175d340d57f45d757962c9f2219
OlenkaPasko <lena_nechipurenko@ukr.net>
2023-03-06 16:53:38 +0200

    GET403

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,16 +1,14 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
-
 const gallery = document.querySelector(".gallery");
 
 const createGallery = galleryItems
   .map(
     ({
       preview,
-      original,
       description,
+      original,
     }) => `<a class="gallery__item" href="${original}">
   <img class="gallery__image" 
   src="${preview} 
@@ -25,20 +23,3 @@ const lightbox = new SimpleLightbox(".gallery a", {
   captionDelay: 250,
   captionPosition: "bottom",
 });
-
-//При використанні окремого варіанту (`simple-lightbox(.min).js`)
-//var lightbox = new SimpleLightbox('.gallery a', { /* options */ });
-//let gallery = new SimpleLightbox('.gallery a');
-//gallery.on('show.simplelightbox', function () {
-// do something…
-//});
-
-//gallery.on('error.simplelightbox', function (e) {
- // some usefull information
-//});
-
-///For the default setup, you just need links that are pointing to images.
-//div class="gallery">
-    //<a href="images/image1.jpg"><img src="images/thumbs/thumb1.jpg" alt="" title=""/></a>
-   // <a href="images/image2.jpg"><img src="images/thumbs/thumb2.jpg" alt="" title="Beautiful Image"/></a>
-//</div>
